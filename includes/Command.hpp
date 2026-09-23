00a7d0951c516d1f5a388791b6e27625578b9a3c
Jeongpil Jeon <jejeon@c2r20s6.42gyeongsan.kr>
2026-09-23 14:15:13 +0900

    fix: handle parser edge cases

--- includes/Command.hpp
+++ includes/Command.hpp
@@ -8,6 +8,7 @@ struct Command
 {
     std::string              name;
     std::vector<std::string> params;
+    std::string              trailing;
 };
 
 #endif
--- srcs/Parser.cpp
+++ srcs/Parser.cpp
@@ -1,5 +1,61 @@
 #include "Parser.hpp"
 
+#include <cctype>
+
+void removeLineEnding(std::string& input)
+{
+	if (input.empty())
+	{
+		return;
+	}
+
+	if (input[input.size() - 1] == '\n')
+	{
+		input.erase(input.size() - 1);
+	} 
+
+	if (!input.empty() && input[input.size() - 1] == '\r')
+	{
+		input.erase(input.size() - 1);
+	}
+}
+
+void preprocessInput(std::string& input)
+{
+	size_t pos = 0;
+
+	while (pos < input.size() && input[pos] == ' ')
+	{
+		pos++;
+	}
+
+	if (pos < input.size() && input[pos] == ':')
+	{
+		while (pos < input.size() && input[pos] != ' ')
+		{
+			pos++;
+		}
+
+		while (pos < input.size() && input[pos] == ' ')
+		{
+			pos++;
+		}
+	}
+
+	input = input.substr(pos);
+}
+
+void normalizeCommand(std::string& input)
+{
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		if (input[i] >= 'a' && input[i] <= 'z')
+		{
+			input[i] = toupper(input[i]);
+		}
+	}
+}
+
 /*
 	IRC 명령어 한 줄을 파싱 합니다.
 	첫 번째 토큰은 명령어 이름으로 저장하고,
@@ -9,44 +65,53 @@
 Command commandParser(const std::string& line)
 {
 	Command result;
-	size_t pos;
-	size_t start;
+	std::string input = line;
 
-	pos = line.find(' ');
-	result.name = line.substr(0, pos);
+	removeLineEnding(input);
+	preprocessInput(input);
+
+	size_t pos = 0;
+
+	pos = input.find(' ');
+	result.name = input.substr(0, pos);
+
+	normalizeCommand(result.name);
+
 	if (pos == std::string::npos)
 	{
 		return (result);
 	}
+	
+	size_t start = pos + 1;
 
-	while (1)
+	while (start < input.size())
 	{
-		start = pos + 1;
-
-		while (start < line.size() && line[start] == ' ')
+		while (start < input.size() && input[start] == ' ')
 		{
 			start++;
-			if (start == line.size())
-			{
-				return (result);
-			}
 		}
 
-		if (line[start] == ':')
+		if (start >= input.size())
 		{
-			result.params.push_back(line.substr(start + 1));
-			return (result);
-		}
-
-		pos = line.find(' ', start);
-
-		if (pos == std::string::npos)
-		{
-			result.params.push_back(line.substr(start));
 			break;
 		}
 
-		result.params.push_back(line.substr(start, pos - start));
+		if (input[start] == ':')
+		{
+			result.trailing = input.substr(start + 1);
+			break;
+		}
+
+		pos = input.find(' ', start);
+
+		if (pos == std::string::npos)
+		{
+			result.params.push_back(input.substr(start));
+			break;
+		}
+
+		result.params.push_back(input.substr(start, pos - start));
+		start = pos + 1;
 	}
 	return (result);
 }
